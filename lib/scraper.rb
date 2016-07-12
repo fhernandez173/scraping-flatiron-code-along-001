1efdaa48cf249cc8f4b5bd6cdf9823ce09a5dc95
fhernandez173 <f.hernandez173@gmail.com>
2016-07-12 12:16:51 -0400

    use Nokogiri/ scraped site

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
-  
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,8 +16,34 @@ class Scraper
       end
     end
   end
-  
+
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    # doc.css(".post").each do |post|
+    #   # doc.css(".post").first.css("h2").text finds what we want for title
+    #   # doc.css(".post").first.css(".date").text finds the schedule
+    #   # doc.css(".post").first.css("p").text finds the description
+    #   course = Course.new
+    #   course.title = post.css("h2").text
+    #   course.schedule = post.css(".date").text
+    #   course.description = post.css("p").text
+    # end
+  end
+  # binding.pry
+
+  def get_courses
+    self.get_page.css(".post")
+  end
+
+  def make_courses
+    self. get_courses.each do |post|
+        course = Course.new
+        course.title = post.css("h2").text
+        course.schedule = post.css(".date").text
+        course.description = post.css("p").text
+    end
+  end
+
 end
 
-
-
+Scraper.new.print_courses
